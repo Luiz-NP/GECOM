074ba5bd58b5ddaf1c10a9833ec0aa684851d221
Luiz <gustavoooliveira111@gmail.com>
2022-12-15 21:16:54 -0300

    adding status bar

--- App.jsx
+++ App.jsx
@@ -1,9 +1,11 @@
+import { StatusBar } from 'react-native';
 import { AuthProvider } from './src/contexts/AuthContext';
 import {Routes} from './src/routes';
 
 export default function App() {
   return (
     <AuthProvider>
+      <StatusBar backgroundColor="transparent" translucent={true} />
       <Routes />
     </AuthProvider>
   );
