4089af72ce77b5759460edbd2576195721e36bee
Luiz <gustavoooliveira111@gmail.com>
2022-12-15 21:11:30 -0300

    wrapping app with auth context

--- App.jsx
+++ App.jsx
@@ -1,5 +1,10 @@
+import { AuthProvider } from './src/contexts/AuthContext';
 import {Routes} from './src/routes';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
+  );
 }
