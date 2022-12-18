9ae97766ecb9be7da0ae809674b5bfd9e5243801
Victor Lima <victorl1ma@hotmail.com>
2022-12-17 23:56:28 -0300

    preparing camera use

---
+++ src/screens/DebugCamera.jsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+
+export default function DebugCamera() {
+  return (
+    <View>
+      <Text>Debug</Text>
+    </View>
+  );
+}
--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -88,10 +88,8 @@ export default function Profile({navigation}) {
               <Path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm.707 13.946l-1.035 1.054h-.672v1h-1v1h-3v-2.292l3.146-3.185c.496 1.111 1.419 1.988 2.561 2.423zm5.293-4.279c0 2.025-1.642 3.667-3.667 3.667-2.024 0-3.666-1.642-3.666-3.667s1.642-3.667 3.666-3.667c2.025 0 3.667 1.642 3.667 3.667zm-1.375-1.375c0-.506-.41-.917-.917-.917s-.916.411-.916.917.409.917.916.917.917-.411.917-.917z" />
             </Svg>
             <View style={styles.optionText}>
-              <Text style={styles.optionTitle}>Alterar senha</Text>
-              <Text style={styles.optionsubTitle}>
-                Escolha uma senha forte e não a reutilize em outras contas.
-              </Text>
+              <Text style={styles.optionTitle}>Testar câmera</Text>
+              <Text style={styles.optionsubTitle}>Debug ok?</Text>
             </View>
           </TouchableOpacity>
           <TouchableOpacity activeOpacity={0.8} style={styles.option}>
