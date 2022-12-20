717da413afcd5ec80e27a305a2aa17b4e368083d
Victor Lima <victorl1ma@hotmail.com>
2022-12-20 16:42:46 -0300

    signout function

--- src/screens/Profile/Profile.jsx
+++ src/screens/Profile/Profile.jsx
@@ -13,6 +13,7 @@ import ProfileSkeleton from '../../Skeletons/ProfileSkeleton.jsx';
 
 import Animated, {FadeInDown, FadeOut} from 'react-native-reanimated';
 import {FadeInUp, FadeOutDown} from 'react-native-reanimated';
+import auth from '@react-native-firebase/auth';
 
 export default function Profile({navigation}) {
   const [Loading, setLoading] = useState(true);
@@ -28,6 +29,15 @@ export default function Profile({navigation}) {
   const displayCompany = 'QuarkzPlace Ltd.';
 
   const isAutorized = true;
+
+  function logOutSession() {
+    navigate('Login');
+    auth()
+      .signOut()
+      .then(() => {
+        console.log('User signed out!');
+      });
+  }
 
   return (
     <View style={global.container}>
@@ -140,6 +150,28 @@ export default function Profile({navigation}) {
               <Text style={styles.optionsubTitle}>
                 Altere seus dados caso estejam inválidos e/ou necessitam de
                 atualização.
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={logOutSession}
+            activeOpacity={0.8}
+            style={styles.option}>
+            <Svg
+              width={36}
+              height={36}
+              viewBox="0 0 20 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <Path
+                d="M12.75 17.5a.75.75 0 000-1.5H6.5a2 2 0 01-2-2V6a2 2 0 012-2h6.25a.75.75 0 000-1.5H6.5A3.5 3.5 0 003 6v8a3.5 3.5 0 003.5 3.5h6.25zm.991-11.301a.75.75 0 011.06.042l3 3.25a.75.75 0 010 1.018l-3 3.25A.75.75 0 1113.7 12.74l1.838-1.991H7.75a.75.75 0 010-1.5h7.787l-1.838-1.991a.75.75 0 01.042-1.06z"
+                fill={'#19E5A6'}
+              />
+            </Svg>
+            <View style={styles.optionText}>
+              <Text style={styles.optionTitle}>Encerrar sessão</Text>
+              <Text style={styles.optionsubTitle}>
+                Finalize sua sessão e volte para a área de autenticação.
               </Text>
             </View>
           </TouchableOpacity>
