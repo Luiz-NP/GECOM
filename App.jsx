bd69e0539a48099c7262e6b7104ec28e622b165f
Luiz <gustavoooliveira111@gmail.com>
2023-01-14 16:09:17 -0300

    good patern

--- App.jsx
+++ App.jsx
@@ -1,12 +1,17 @@
 import {StatusBar} from 'react-native';
 import {AuthProvider} from './src/contexts/AuthContext';
+import {DataProvider} from './src/contexts/DataContext';
 import {Routes} from './src/routes';
 
-export default function App() {
+const App = () => {
   return (
     <AuthProvider>
       <StatusBar backgroundColor="transparent" translucent={true} />
-      <Routes />
+      <DataProvider>
+        <Routes />
+      </DataProvider>
     </AuthProvider>
   );
 }
+
+export default App;
