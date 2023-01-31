c9fd892fcfabbede750b0bccef783c4cc776c21f
Luiz <gustavoooliveira111@gmail.com>
2023-01-31 11:21:45 -0300

    adding google sign-in

--- src/screens/Auth.tsx
+++ src/screens/Auth.tsx
@@ -12,8 +12,10 @@ import {
 import Svg, {Defs, Path, ClipPath, Use} from 'react-native-svg';
 import LottieView from 'lottie-react-native';
 
-// firebase Auth
+// auth
 import auth from "@react-native-firebase/auth";
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import "../configs/google.config";
 
 export const Auth = ({navigation}: any): JSX.Element => {
   const {navigate} = navigation;
@@ -44,6 +46,21 @@ export const Auth = ({navigation}: any): JSX.Element => {
         console.error(error);
       });
   };
+
+  // google sign-in
+  const onGoogleButtonPress = async () => {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
+
+    // Get the users ID token
+    const {idToken} = await GoogleSignin.signIn();
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  }
 
   return (
     <View style={styles.authContainer}>
@@ -95,7 +112,15 @@ export const Auth = ({navigation}: any): JSX.Element => {
           maxLength={8}
           style={styles.input}></TextInput>
         <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-          <TouchableOpacity style={styles.googleBtn} activeOpacity={0.8}>
+          <TouchableOpacity 
+            onPress={() =>
+              onGoogleButtonPress().then(() => {
+                console.log('Signed in with Google!');
+                navigate('Home');
+            }).catch(err => console.log(err))
+            }
+            style={styles.googleBtn} 
+            activeOpacity={0.8}>
             <Svg viewBox="0 0 32 32" width={36} height={36}>
               <Defs>
                 <Path
