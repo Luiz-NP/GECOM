9c3840b9f5b5890f9ffea5237383ac3ee03a0d07
Victor Lima <trackedby1@gmail.com>
2023-02-03 17:40:44 -0300

    fixed layout in home screen and added some icons, starting to recolorize all and put fonts to work!

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -8,6 +8,7 @@ import {
   ScrollView,
 } from 'react-native';
 import {useContext, useEffect, useState} from 'react';
+import Svg, {Defs, Path, ClipPath, Use} from 'react-native-svg';
 
 /*========== FIREBASE IMPORTS ==========*/
 import firestore from '@react-native-firebase/firestore';
@@ -76,12 +77,38 @@ export function Home({navigation}) {
       <View style={styles.header}>
         <View>
           <Text style={styles.titleScreen}>Início</Text>
-          <Text style={styles.subTitleScreen}>Suas Tarefas</Text>
+          <Text style={styles.subTitleScreen}>Suas tarefas</Text>
         </View>
 
         <View style={styles.icons}>
-          <View style={styles.icon}></View>
-          <View style={styles.icon}></View>
+          <View style={styles.icon}>
+            <Svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 20.996 20.995">
+              <Path
+                id="Account"
+                d="M13,14.049V14H4.253A2.249,2.249,0,0,0,2,16.248v.92a2.75,2.75,0,0,0,.51,1.6C4.056,20.928,6.579,22,10,22q.536,0,1.043-.035A2.518,2.518,0,0,1,11,21.5v-5A2.5,2.5,0,0,1,13,14.049ZM10,2A5,5,0,1,1,5,7,5,5,0,0,1,10,2Zm4,13h-.5A1.5,1.5,0,0,0,12,16.5v5A1.5,1.5,0,0,0,13.5,23h8A1.5,1.5,0,0,0,23,21.5v-5A1.5,1.5,0,0,0,21.5,15H21v-1.25A1.75,1.75,0,0,0,19.25,12h-3.5A1.75,1.75,0,0,0,14,13.749Zm1.5-1.25a.25.25,0,0,1,.25-.25h3.5a.25.25,0,0,1,.25.25V15h-4Z"
+                transform="translate(-2.004 -2.004)"
+                fill="#fff"
+              />
+            </Svg>
+          </View>
+          <View style={styles.icon}>
+            <Svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 18.724 19.5">
+              <Path
+                id="Settings"
+                d="M12.013,2.25a10.583,10.583,0,0,1,2.182.253.75.75,0,0,1,.582.649l.17,1.527a1.384,1.384,0,0,0,1.375,1.233,1.358,1.358,0,0,0,.553-.117l1.4-.615a.75.75,0,0,1,.849.174,9.793,9.793,0,0,1,2.2,3.792.75.75,0,0,1-.271.826l-1.241.915a1.381,1.381,0,0,0,0,2.226l1.243.915a.75.75,0,0,1,.271.826,9.8,9.8,0,0,1-2.2,3.792.75.75,0,0,1-.849.175L16.872,18.2a1.381,1.381,0,0,0-1.926,1.113l-.169,1.527a.75.75,0,0,1-.572.647,9.518,9.518,0,0,1-4.406,0,.75.75,0,0,1-.572-.647L9.058,19.32a1.382,1.382,0,0,0-1.925-1.111l-1.407.616a.75.75,0,0,1-.849-.175,9.8,9.8,0,0,1-2.2-3.8.75.75,0,0,1,.272-.826l1.243-.916a1.381,1.381,0,0,0,0-2.226L2.946,9.973a.75.75,0,0,1-.272-.826,9.793,9.793,0,0,1,2.2-3.792.75.75,0,0,1,.849-.174l1.4.615A1.388,1.388,0,0,0,9.059,4.678l.17-1.526A.75.75,0,0,1,9.812,2.5,10.665,10.665,0,0,1,12.013,2.25ZM12,9a3,3,0,1,0,3,3A3,3,0,0,0,12,9Z"
+                transform="translate(-2.641 -2.25)"
+                fill="#fff"
+              />
+            </Svg>
+          </View>
         </View>
       </View>
 
@@ -159,11 +186,10 @@ const styles = StyleSheet.create({
   },
 
   header: {
-    minHeight: 200,
-    paddingBottom: 30,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'flex-end',
+    alignItems: 'center',
+    marginTop: 72,
   },
   titleScreen: {
     fontFamily: 'ClashGrotesk-Medium',
@@ -172,20 +198,20 @@ const styles = StyleSheet.create({
   },
   subTitleScreen: {
     color: '#FFFFFF',
-    fontSize: 24,
+    fontSize: 32,
     fontFamily: 'ClashGrotesk-Medium',
   },
   icons: {
     flexDirection: 'row',
-    width: '35%',
     justifyContent: 'space-between',
+    width: '25%',
   },
 
   icon: {
-    backgroundColor: '#FFFFFF70',
-    borderRadius: 20,
-    width: 50,
-    height: 50,
+    width: 32,
+    height: 32,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 
   nav: {
