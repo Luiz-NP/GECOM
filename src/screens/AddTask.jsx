1fd3b0d08f36a675afc75e5c4f7fe0676f068f76
Luiz <gustavoooliveira111@gmail.com>
2023-02-06 09:01:48 -0300

    filling addTaks screen

--- src/screens/AddTask.jsx
+++ src/screens/AddTask.jsx
@@ -25,18 +25,23 @@ export function AddNewTask({route, navigation}) {
 
   /*========== STATES ==========*/ 
   const [location, setLocation] = useState('');
-
+  const [cableCount, setCableCount] = useState(0);
+  const [cableType, setCableType] = useState('');
+  const [allCables, setAllCables] = useState([]);
+  const [poles, setPoles] = useState('0');
+    
   /*========== CONTEXTS ==========*/
   const {update, setUpdate} = useContext(UpdateContext)
 
   /*========== FUNCTIONS ==========*/
   // adding new task when user click on add task button
-  const handleAddNewTask = () => {
+  function handleAddNewTask(target) {
     // getting current user id
     const uid = auth().currentUser.uid;
 
-    if (location === '') return Alert.alert('Preencha os Campos');
+    if (location === '' || cableCount === '' || cableType === '' || poles < 1) return Alert.alert('Preencha todos os campos, de forma válida');
 
+    console.log(allCables);
     firestore()
       .collection('Tasks')
       .doc(uid)
@@ -45,13 +50,15 @@ export function AddNewTask({route, navigation}) {
           id: taskID,
           distance: 0,
           location: location,
+          cables: allCables,
+          poles: poles,
           status: 'pending',
         }),
       })
       .then(() => {
         console.log('task created!');
         setUpdate(!update);
-        navigate('Home');
+        navigate(target);
       })
       .catch(error => {
         console.log(error)
@@ -66,13 +73,15 @@ export function AddNewTask({route, navigation}) {
                 id: taskID,
                 distance: 0,
                 location: location,
+                cables: allCables,
+                poles: poles,
                 status: 'pending',
               }]
             })
             .then(() => {
               console.log('task created!');
               setUpdate(!update);
-              navigate('Home');
+              navigate(target);
             })
             .catch(error => console.log(error));
         }
@@ -95,15 +104,46 @@ export function AddNewTask({route, navigation}) {
       </View>
 
       <View>
+        <Text style={styles.label}>Local da Inspeção</Text>
         <TextInput
           onChangeText={text => setLocation(text)}
           style={styles.input}
-          placeholder="Localização"
+          placeholder="Ex: São Paulo - SP"
+        />
+        <Text style={styles.label}>Variedade de Cabos</Text>
+        <TextInput
+          onChangeText={text => setCableCount(text)}
+          style={styles.input}
+          keyboardType="numeric"
+          maxLength={1}
+          placeholder="Ex: 2"
+        />
+        {Array.from({ length: cableCount }).map((value, index) => (
+            <View key={index}>
+               <Text style={styles.label}>Tipo do Cabo {index + 1}</Text>
+               <TextInput
+                 onChangeText={text => setCableType(text)}
+                 onBlur={() => setAllCables(prev => [...prev, {id: index + 1, type: cableType}])}
+                 style={styles.input}
+                 placeholder="Ex: Cat5e"
+               />
+            </View>
+        ))}
+        <Text style={styles.label}>Quantidade de Postes</Text>
+        <TextInput
+          onChangeText={text => setPoles(text)}
+          style={styles.input}
+          keyboardType="numeric"
+          maxLength={2}
+          placeholder="Ex: 4"
         />
       </View>
 
-      <TouchableOpacity onPress={handleAddNewTask} style={styles.addTaskButton}>
-        <Text style={styles.addTaskText}>adicionar tarefa</Text>
+      <TouchableOpacity onPress={() => handleAddNewTask('Home')} style={styles.addTaskButton}>
+        <Text style={styles.addTaskText}>adicionar e voltar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => handleAddNewTask('Camera')} style={styles.initTask}>
+        <Text style={styles.initTaskText}>iniciar inspeção</Text>
       </TouchableOpacity>
     </View>
   );
@@ -135,6 +175,11 @@ const styles = StyleSheet.create({
   form: {
     marginVertical: 24,
   },
+  label: {
+    marginLeft: 12,
+    fontFamily: 'ClashGrotesk-Medium',
+    color: '#fff',
+  },
   input: {
     width: '100%',
     height: 70,
@@ -146,6 +191,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: 6,
+    marginBottom: 12,
     fontFamily: 'ClashGrotesk-Medium',
     color: '#fff',
     fontSize: 24,
@@ -156,13 +202,28 @@ const styles = StyleSheet.create({
     paddingVertical: 8,
     borderWidth: 1,
     borderColor: '#00c4ac',
-    marginTop: 24,
-    marginBottom: 96,
+    marginTop: 12,
   },
 
   addTaskText: {
     textAlign: 'center',
     textTransform: 'uppercase',
     color: '#00c4ac',
+  },
+    
+  initTask: {
+    width: '100%',
+    borderRadius: 15,
+    paddingVertical: 8,
+    borderWidth: 1,
+    borderColor: '#00c4ac',
+    backgroundColor: '#00c4ac',
+    marginTop: 24,
+  },
+    
+  initTaskText: {
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    color: '#fff',
   },
 });
