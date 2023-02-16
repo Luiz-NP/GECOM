e5af50ce160ba670f4e041fafaa9b9f37750434c
Luiz-NP <gustavoooliveira111@gmail.com>
2023-02-16 20:29:18 -0300

    getting user current location
    Co-authored-by: Marcos-Segantine <msegantine8@gmail.com>

--- src/components/CameraView.jsx
+++ src/components/CameraView.jsx
@@ -1,18 +1,32 @@
 import {Camera, useCameraDevices} from 'react-native-vision-camera';
-import {Image, Pressable, View} from 'react-native';
-import {useEffect, useRef, useState} from 'react';
+import {
+  Image,
+  Pressable,
+  View,
+  Button,
+  PermissionsAndroid,
+  Text,
+} from 'react-native';
+import {useEffect, useRef, useState, useContext} from 'react';
+import RNAndroidLocationEnabler from 'react-native-android-location-enabler';
 
 import iconCamera from '../assets/camera/capture-icon.png';
+
+import {DataContext} from '../contexts/DataContext';
+import Geolocation from '@react-native-community/geolocation';
 
 import RNFS from 'react-native-fs';
 
 export function CameraView() {
   const [device, setDevice] = useState();
   const [photo, setPhoto] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const devices = useCameraDevices('wide-angle-camera');
+  const {setData, distance} = useContext(DataContext);
 
-  const camera = useRef(null);
+  const camera = useRef();
 
   useEffect(() => {
     setDevice(devices.back);
@@ -20,14 +34,45 @@ export function CameraView() {
     (async () => {
       const newCameraPermission = await Camera.requestCameraPermission();
 
+      PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      );
+
+      await RNAndroidLocationEnabler.promptForEnableLocationIfNeeded({
+        interval: 10000,
+        fastInterval: 5000,
+      });
+
       console.log(newCameraPermission);
     })();
   }, [devices]);
+
+  const getLocation = async () => {
+    Geolocation.getCurrentPosition(
+      info => {
+        const coordinates = {
+          latitude: info.coords.latitude,
+          longitude: info.coords.longitude,
+        };
+
+        console.log(coordinates);
+        setLocation(coordinates);
+        setLoading(false);
+      },
+      error => {
+        console.log(error);
+      },
+      {enableHighAccuracy: false, timeout: 200000, maximumAge: 1000},
+    );
+  };
 
   const takePhoto = async () => {
     const photo = await camera.current.takePhoto({
       flash: 'on',
     });
+
+    setLoading(true);
+    await getLocation();
 
     const pic = await RNFS.readFile(photo.path, 'base64').then(res => {
       return res;
@@ -35,12 +80,40 @@ export function CameraView() {
     setPhoto(pic);
   };
 
-  if (photo) {
+  // loading state
+  if (loading) {
+    return (
+      <View
+        style={{
+          height: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: 'black',
+        }}>
+        <Text style={{color: 'white'}}>Carregando...</Text>
+      </View>
+    );
+  }
+
+  if (photo && location) {
     return (
       <View>
         <Image
-          style={{width: '100%', height: '100%'}}
+          style={{width: '100%', height: '92%'}}
           source={{uri: 'data:image/jpeg;base64,' + photo}}
+        />
+        <Button
+          onPress={() => {
+            const data = {
+              photo: photo,
+              location: location,
+            };
+
+            setData(prev => [...prev, data]);
+            setLocation(null);
+            setPhoto(null);
+          }}
+          title="continuar"
         />
       </View>
     );
@@ -52,6 +125,15 @@ export function CameraView() {
         alignItems: 'center',
         justifyContent: 'center',
       }}>
+      <Text
+        style={{
+          position: 'absolute',
+          top: 100,
+          zIndex: 3,
+          backgroundColor: 'black',
+        }}>
+        {distance} metros percorridos
+      </Text>
       {device ? (
         <Camera
           style={{
@@ -68,6 +150,7 @@ export function CameraView() {
 
       <Pressable
         style={{
+          bottom: 60,
           position: 'absolute',
         }}
         onPress={takePhoto}>
