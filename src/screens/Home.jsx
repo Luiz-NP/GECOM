a59df89a3c71e1aa1fc17ef91eb1cedb452379d9
Victor Lima <trackedby1@gmail.com>
2023-03-01 19:56:37 -0300

    fixed button not showing in home

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -215,6 +215,33 @@ export function Home({navigation}) {
           />
         )}
       />
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onPress={() => {
+          const taskID = tasks?.length ?? 0;
+          navigate('AddNewTask', {taskID: taskID + 1});
+
+          // set button selected to 0
+          setTimeout(() => setButtonSelected(0), 1000);
+        }}
+        style={[
+          buttonSelected === 1 || buttonSelected === 2
+            ? styles.buttonHidden
+            : styles.addTaskButton,
+          loading === true ? styles.buttonHidden : null,
+        ]}>
+        <Svg
+          width={32}
+          height={32}
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg">
+          <Path
+            d="M11.883 3.007L12 3a1 1 0 01.993.883L13 4v7h7a1 1 0 01.993.883L21 12a1 1 0 01-.883.993L20 13h-7v7a1 1 0 01-.883.993L12 21a1 1 0 01-.993-.883L11 20v-7H4a1 1 0 01-.993-.883L3 12a1 1 0 01.883-.993L4 11h7V4a1 1 0 01.883-.993L12 3l-.117.007z"
+            fill="#00c4ac"
+          />
+        </Svg>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -333,7 +360,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderColor: '#00c4ac',
     marginTop: 24,
-    marginBottom: 96,
+    marginBottom: 48,
   },
 
   buttonHidden: {
