56e77327fc88e3e394f68a6c74bf74bbe822509c
Luiz-NP <gustavoooliveira111@gmail.com>
2023-03-04 14:35:41 -0300

    shifting calculation responsibility to another component

--- src/contexts/DataContext.jsx
+++ src/contexts/DataContext.jsx
@@ -1,33 +1,12 @@
-import {createContext, useEffect, useState} from 'react';
-import {getPreciseDistance} from 'geolib';
+import {createContext, useState} from 'react';
 
 export const DataContext = createContext();
 
 export const DataProvider = ({children}) => {
   const [data, setData] = useState([]);
-  const [distance, setDistance] = useState(0);
-
-  useEffect(() => {
-    console.log(data.length);
-
-    if (data.length >= 2) {
-      let meters = 0;
-
-      for (let count = 0; count <= data.length - 2; count++) {
-        meters += getPreciseDistance(
-          data[count].location,
-          data[count + 1].location,
-        );
-      }
-
-      console.log(meters);
-
-      setDistance(meters);
-    }
-  }, [data]);
 
   return (
-    <DataContext.Provider value={{data, setData, distance, setDistance}}>
+    <DataContext.Provider value={{data, setData}}>
       {children}
     </DataContext.Provider>
   );
