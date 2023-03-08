3795f43489c5d246fc86a9f49c645e0baa702609
Luiz-NP <gustavoooliveira111@gmail.com>
2023-03-08 16:08:29 -0300

    adjusting functions

--- src/utils/addTask.js
+++ src/utils/addTask.js
@@ -1,6 +1,5 @@
-export const addTask = (tasks, navigate, setButtonSelected) => {
-    const taskID = tasks?.length ?? 0;
-    navigate('AddNewTask', { taskID: taskID + 1 });
+export const addTask = (navigate, setButtonSelected) => {
+    navigate('AddNewTask');
 
     // set button selected to 0
     setTimeout(() => setButtonSelected(0), 1000);
