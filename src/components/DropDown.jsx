bd0644e4ecfbc595f0eb524300e001546314a3c8
Luiz-NP <gustavoooliveira111@gmail.com>
2023-03-11 20:32:17 -0300

    adding new dropdown

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,75 +1,58 @@
-import {useState} from 'react';
-import {StyleSheet, TouchableOpacity, Text} from 'react-native';
+import {useState} from "react"
+import DropDownPicker from 'react-native-dropdown-picker';
 
-export const DropDown = ({options, value, setValue}) => {
-  const [dropdown, setDropdown] = useState(false);
-  
-  const data = {};
-  console.log(data)
+export const DropDown = ({items, setItems, value, setValue, multiple}) => {
+  const [open, setOpen] = useState(false);
+
+  console.log(value)
 
   return (
-    <>
-      <TouchableOpacity
-        activeOpacity={0.8}
-        onPress={() => setDropdown(!dropdown)}
-        style={styles.select}>
-        <Text style={styles.optionText}>
-          {value ?? 'Selecione'}
-        </Text>
-      </TouchableOpacity>
-
-      {/* {dropdown &&
-        id &&
-        options.map((value, index) => (
-          <TouchableOpacity
-            activeOpacity={0.8}
-            key={index}
-            onPress={() => {
-              setDropdown(!dropdown);
-              data[`cable${index+1}`] = value;
-            }}
-            style={styles.option}>
-            <Text style={styles.optionText}>{value}</Text>
-          </TouchableOpacity>
-        ))} */}
-
-      {dropdown && options.map((value, index) => (
-          <TouchableOpacity
-            activeOpacity={0.8}
-            key={index}
-            onPress={() => {
-              setDropdown(!dropdown);
-              setValue(value)
-            }}
-            style={styles.option}>
-            <Text style={styles.optionText}>{value}</Text>
-          </TouchableOpacity>
-        )
-      )}
-    </>
+    <DropDownPicker
+      open={open}
+      value={value}
+      items={items}
+      setOpen={setOpen}
+      setValue={setValue}
+      setItems={setItems}
+      multiple={multiple}
+      theme="DARK"
+      mode="BADGE"
+      listMode="SCROLLVIEW"
+      scrollViewProps={{
+        nestedScrollEnabled: true
+      }}
+      badgeDotColors={["#e76f51", "#00b4d8", "#e9c46a", "#e76f51", "#8ac926", "#00b4d8", "#e9c46a"]}
+      searchable
+      searchPlaceholder="Pesquisar. . ."
+      style={{
+        backgroundColor: '#1e1e1e',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 15,
+        borderColor: '#1e1e1e',
+      }}
+      textStyle={{
+        color: 'white',
+        fontFamily: 'ClashGrotesk-Medium',
+        fontSize: 16,
+      }}
+      dropDownContainerStyle={{
+        backgroundColor: '#1e1e1e',
+        borderColor: '#1e1e1e',
+        position: "relative",
+        top: 0,
+      }}
+      badgeTextStyle={{
+        color: 'black',
+        fontFamily: 'ClashGrotesk-Medium',
+        fontSize: 10,
+      }}
+      placeholder="Selecione"
+      placeholderStyle={{
+        color: "#444"
+      }}
+      
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  select: {
-    backgroundColor: '#1e1e1e',
-    borderRadius: 10,
-    paddingHorizontal: 12,
-    paddingVertical: 12,
-  },
-
-  optionText: {
-    color: '#ccc',
-    fontSize: 16,
-    fontFamily: 'ClashGrotesk-Medium',
-  },
-
-  option: {
-    backgroundColor: '#1e1e1e',
-    borderRadius: 10,
-    paddingHorizontal: 12,
-    paddingVertical: 12,
-    borderWidth: 1,
-    borderColor: '#2e2e2e',
-  },
-});
