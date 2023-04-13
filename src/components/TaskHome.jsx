35a176bb2a20cc10d94ed1f5f12aea485eb4b314
Victor Lima <trackedby1@gmail.com>
2023-04-13 10:17:54 -0300

    dynamic identify finished task

--- src/components/TaskHome.jsx
+++ src/components/TaskHome.jsx
@@ -21,32 +21,55 @@ export function TaskHome({data, setModalData, navigate, setModal}) {
     meters = `${meters} metros`;
   }
 
+  console.log(data.status);
   return (
     <TouchableOpacity
       activeOpacity={0.8}
-      style={styles.task}
+      style={data.status === 'completed' ? styles.taskFinished : styles.task}
       onPress={() =>
         navigate('TaskInfo', {
           data,
         })
       }>
-      <Text style={styles.ownerTask}>{data.company}</Text>
-      <Text style={styles.textTask}>{taskType}</Text>
+      <Text
+        style={
+          data.status === 'completed'
+            ? styles.ownerTaskFinished
+            : styles.ownerTask
+        }>
+        {data.company}
+      </Text>
+      <Text
+        style={
+          data.status === 'completed'
+            ? styles.textTaskFinished
+            : styles.textTask
+        }>
+        {taskType}
+      </Text>
 
       <View style={styles.contentInfo}>
-        <View style={styles.info}>
+        <View
+          style={
+            data.status === 'completed' ? styles.infoFinished : styles.info
+          }>
           <Text style={styles.textInfo}>
             {meters === 'Não iniciada' ? meters : meters}
           </Text>
         </View>
-        <View style={styles.info}>
+        <View
+          style={
+            data.status === 'completed' ? styles.infoFinished : styles.info
+          }>
           <Text style={styles.textInfo}>{data.location.city}</Text>
         </View>
       </View>
 
       <TouchableOpacity
         activeOpacity={0.8}
-        style={styles.deleteTask}
+        style={
+          data.status === 'completed' ? {display: 'none'} : styles.deleteTask
+        }
         onPress={handleConfirmationWantDeleteTask}>
         <GarbageIcon width={32} height={32} />
       </TouchableOpacity>
@@ -77,6 +100,39 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontFamily: 'ClashGrotesk-Medium',
     color: '#00c4ac',
+  },
+  taskFinished: {
+    borderWidth: 1,
+    borderColor: '#15796D',
+    borderRadius: 15,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginTop: 15,
+    height: 120,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+
+  textTaskFinished: {
+    fontSize: 24,
+    fontFamily: 'ClashGrotesk-Medium',
+    color: 'white',
+    marginBottom: 6,
+  },
+
+  ownerTaskFinished: {
+    fontSize: 16,
+    fontFamily: 'ClashGrotesk-Medium',
+    color: '#15796D',
+  },
+
+  infoFinished: {
+    backgroundColor: '#15796D',
+    borderRadius: 20,
+    alignSelf: 'flex-start',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginRight: 10,
   },
 
   contentInfo: {
